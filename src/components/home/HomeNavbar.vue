575d887f0ade584244189f1c2dc5fc4ca35091e8
Cenk Bülbül <cenkbulbul67@gmail.com>
2023-01-03 21:27:42 +0300

    vue3-online-sergi-app

---
+++ src/components/home/HomeNavbar.vue
@@ -0,0 +1,63 @@
+<template>
+	<nav class="deep-purple lighten-3">
+		<div>
+			<router-link :to="{name:'SergiListesi'}" class="brand-logo">
+				AOS Online Sergi
+			</router-link>
+			<ul class="right">
+				<li>
+					<a @click="handleGiris" class="waves-effect waves-light btn-flat">
+						<i class="fab fa-google-plus-g"></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</template>
+<script>
+	import {firebase,authRef,firestoreRef} from "@/firebase/config"
+	import {useRouter} from "vue-router"
+
+	export default{
+		setup(){
+
+			const router= useRouter()
+
+			const handleGiris = async()=>{
+				const provider = new firebase.auth.GoogleAuthProvider()
+				//console.log(provider)
+
+				await authRef.signInWithPopup(provider)
+				.then(async()=>{
+					//console.log(authRef.currentUser)
+					const {uid,displayName,email,photoURL}=authRef.currentUser
+
+					const snapshot = await firestoreRef.collection("kullanicilar").where("uid","==",uid).get()
+					//console.log(snapshot)
+
+					if (snapshot.empty) {
+						console.log("kullanıcı kaydı bulunamadı")
+						const yeniKullanici={
+							uid,
+							kullaniciAd:displayName,
+							email,
+							fotoURL:photoURL,
+							yetki:"yok"
+						}
+						firestoreRef.collection("kullanicilar").add(yeniKullanici).then(()=>{
+							router.push({name:"Profil"})
+						})
+					}else{
+						console.log("kullancı bulundu")
+						router.push({name:"Profil"})
+					}
+				})
+			}
+
+			return{handleGiris}
+		}
+	}
+</script>
+<style>
+	
+</style>
---
+++ src/views/admin/Admin.vue
@@ -0,0 +1,14 @@
+<template>
+  <Navbar />
+  <div class="container">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/admin/AdminNavbar'
+export default {
+
+  components:{Navbar}
+}
+</script>
